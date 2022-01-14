3ce5c2c5886b361dc8417199b3924c9e171483d4
marvinraydalida <marvinraydalida@gmail.com>
2022-01-15 06:39:02 +0800

    Added error hover to registration

--- application/views/register.php
+++ application/views/register.php
@@ -16,48 +16,53 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<div class="form-container">
 		<form method="POST">
 			<div class="register-input">
-				<?php if (empty(form_error('email'))) : ?>
-					<input type="text" name="email" placeholder="email">
-				<?php else : ?>
-					<input type="text" name="email" placeholder="email" class="error-border">
-				<?php endif; ?>
+				<div class="input-container">
+					<?php if (empty(form_error('email'))) : ?>
+						<input type="text" name="email" placeholder="email">
+					<?php else : ?>
+						<input type="text" name="email" placeholder="email" class="error-border">
+						<span><?php echo form_error('email'); ?></span>
+					<?php endif; ?>
+				</div>
 
 				<div class="name">
 					<?php if (empty(form_error('first_name'))) : ?>
 						<input type="text" name="first_name" placeholder="First name">
 					<?php else : ?>
 						<input type="text" name="first_name" placeholder="First name" class="error-border">
+						<span><?php echo form_error('first_name') . form_error('last_name'); ?></span>
 					<?php endif; ?>
 
 					<?php if (empty(form_error('last_name'))) : ?>
 						<input type="text" name="last_name" placeholder="Last name">
 					<?php else : ?>
 						<input type="text" name="last_name" placeholder="Last name" class="error-border">
+						<span><?php echo form_error('first_name') . form_error('last_name'); ?></span>
 					<?php endif; ?>
 				</div>
-				<?php if (empty(form_error('password'))) : ?>
-					<input type="password" name="password" placeholder="password">
-				<?php else : ?>
-					<input type="text" name="password" placeholder="Password" class="error-border">
-				<?php endif; ?>
+				<div class="input-container">
+					<?php if (empty(form_error('password'))) : ?>
+						<input type="password" name="password" placeholder="password">
+					<?php else : ?>
+						<input type="password" name="password" placeholder="Password" class="error-border">
+						<span><?php echo form_error('password'); ?></span>
+					<?php endif; ?>
 
-				<?php if (empty(form_error('cpassword'))) : ?>
-					<input type="password" name="cpassword" placeholder="Confirm password">
-				<?php else : ?>
-					<input type="text" name="cpassword" placeholder="Confirm password" class="error-border">
-				<?php endif; ?>
+				</div>
+				<div class="input-container">
+					<?php if (empty(form_error('cpassword'))) : ?>
+						<input type="password" name="cpassword" placeholder="Confirm password">
+					<?php else : ?>
+						<input type="password" name="cpassword" placeholder="Confirm password" class="error-border">
+						<span><?php echo form_error('cpassword'); ?></span>
+					<?php endif; ?>
+				</div>
 			</div>
 			<div class="register-button">
 				<input type="submit" value="submit">
 			</div>
 		</form>
 	</div>
-	<?php if (!empty(validation_errors())) : ?>
-		<div class="error">
-			<?php echo validation_errors(); ?>
-		</div>
-	<?php endif; ?>
-
 </body>
 
 </html>
