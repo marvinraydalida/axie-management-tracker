988f43f4cb7066b7faaac12de4add6bd159eeaf3
marvinraydalida <marvinraydalida@gmail.com>
2022-01-17 05:22:42 +0800

    Added currency fetch interval every 30 seconds

--- assets/javascript/currency.js
+++ assets/javascript/currency.js
@@ -1,34 +1,42 @@
-fetch('https://api.coinbase.com/v2/exchange-rates?currency=ETH')
-    .then(data => {
-        return data.json();
-    })
-    .then(post => {
-        const money_format = new Intl.NumberFormat('en-US',
-            { style: 'currency', currency: 'PHP' }
-        ).format(post.data.rates.PHP);
-        document.querySelector('.eth').children[1].innerHTML = money_format;
-    });
+function fetch_currencies() {
+    fetch('https://api.coinbase.com/v2/exchange-rates?currency=ETH')
+        .then(data => {
+            return data.json();
+        })
+        .then(post => {
+            const money_format = new Intl.NumberFormat('en-US',
+                { style: 'currency', currency: 'PHP' }
+            ).format(post.data.rates.PHP);
+            document.querySelector('.eth').children[1].innerHTML = money_format;
+            console.log('eth');
+        });
 
 
-fetch('https://api.coinbase.com/v2/exchange-rates?currency=AXS')
-    .then(data => {
-        return data.json();
-    })
-    .then(post => {
-        const money_format = new Intl.NumberFormat('en-US',
-            { style: 'currency', currency: 'PHP' }
-        ).format(post.data.rates.PHP);
-        document.querySelector('.axs').children[1].innerHTML = money_format;
-    });
+    fetch('https://api.coinbase.com/v2/exchange-rates?currency=AXS')
+        .then(data => {
+            return data.json();
+        })
+        .then(post => {
+            const money_format = new Intl.NumberFormat('en-US',
+                { style: 'currency', currency: 'PHP' }
+            ).format(post.data.rates.PHP);
+            document.querySelector('.axs').children[1].innerHTML = money_format;
+            console.log('axs');
+        });
 
 
-fetch('https://api.coinbase.com/v2/exchange-rates?currency=SLP')
-    .then(data => {
-        return data.json();
-    })
-    .then(post => {
-        const money_format = new Intl.NumberFormat('en-US',
-            { style: 'currency', currency: 'PHP' }
-        ).format(post.data.rates.PHP);
-        document.querySelector('.slp').children[1].innerHTML = money_format;
-    });
+    fetch('https://api.coinbase.com/v2/exchange-rates?currency=SLP')
+        .then(data => {
+            return data.json();
+        })
+        .then(post => {
+            const money_format = new Intl.NumberFormat('en-US',
+                { style: 'currency', currency: 'PHP', minimumFractionDigits: 4, }
+            ).format(post.data.rates.PHP);
+            document.querySelector('.slp').children[1].innerHTML = money_format;
+            console.log('slp');
+        });
+}
+
+fetch_currencies();
+setInterval(fetch_currencies, 30 * (1000));
